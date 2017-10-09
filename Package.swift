bab9179adec7f2cf2d41e4da029a3939f77f9fbf
favre <thomas.favre@save.co>
2017-10-09 13:53:02 +0200

    feat: add poutouPush target

--- Package.swift
+++ Package.swift
@@ -5,9 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "PoutouPush",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PoutouPush",
+            targets: ["PoutouPush"]
+        )
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/IBM-Swift/CCurl.git", from:"0.4.1")
+      .package(url: "https://github.com/IBM-Swift/CCurl.git", from: "0.4.1")
+    ],
+    targets: [
+        .target(name: "PoutouPush",
+        dependencies: [])
     ]
 )
